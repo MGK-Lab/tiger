a5f0094ab1b94ddb1fd526b378bb79f15f0d5794
Maziar Korzani <m.gh.korzani@gmail.com>
2017-10-11 14:51:26 +0200

    initialising the project

--- src/main.C
+++ src/main.C
@@ -1,4 +1,4 @@
-#include "TigerTestApp.h"
+#include "TigerApp.h"
 #include "MooseInit.h"
 #include "Moose.h"
 #include "MooseApp.h"
@@ -15,10 +15,10 @@ main(int argc, char * argv[])
   MooseInit init(argc, argv);
 
   // Register this application's MooseApp and any it depends on
-  TigerTestApp::registerApps();
+  TigerApp::registerApps();
 
   // Create an instance of the application and store it in a smart pointer for easy cleanup
-  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("TigerTestApp", argc, argv);
+  std::shared_ptr<MooseApp> app = AppFactory::createAppShared("TigerApp", argc, argv);
 
   // Execute the application
   app->run();
