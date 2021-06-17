36513bb1f4401f4b6870695c4fc440fbfde5dd61
Maziar Korzani <m.gh.korzani@gmail.com>
2021-06-17 17:42:55 +0200

    hydraulic feedback from mechanics

--- include/kernels/TigerHydroMechanicsKernelHM.h
+++ include/kernels/TigerHydroMechanicsKernelHM.h
@@ -23,45 +23,34 @@
 
 #pragma once
 
-#include "Material.h"
+#include "Kernel.h"
+#include "RankTwoTensor.h"
 
-class TigerMechanicsMaterialM;
+class TigerHydroMechanicsKernelHM;
 
 template <>
-InputParameters validParams<TigerMechanicsMaterialM>();
+InputParameters validParams<TigerHydroMechanicsKernelHM>();
 
-class TigerMechanicsMaterialM : public Material
+class TigerHydroMechanicsKernelHM : public Kernel
 {
 public:
-  TigerMechanicsMaterialM(const InputParameters & parameters);
+  TigerHydroMechanicsKernelHM(const InputParameters & parameters);
 
 protected:
-  virtual void computeQpProperties() override;
+  virtual Real computeQpResidual() override;
+  virtual Real computeQpJacobian() override;
+  virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
 
-  // bulk density
-  MaterialProperty<Real> & _rho_b;
-  // biot coefficient for poromechanics
-  MaterialProperty<Real> & _biot;
-
-  // Imported props from TigerGeometryMaterial
-  const MaterialProperty<Real> & _n;
-
-  /// Number of displacements supplied (1 in 1D, 2 in 2D, 3 in 3D)
-  const unsigned int _ndisp;
-
-  /// Gradient of the displacements
-  std::vector<const VariableGradient *> _grad_disp;
-
-  /// Old value of gradient of the displacements
-  std::vector<const VariableGradient *> _grad_disp_old;
-
-  /// The volumetric strain rate at the quadpoints
-  MaterialProperty<Real> & _vol_strain_rate;
-
-  /// The total volumetric strain at the quadpoints
-  MaterialProperty<Real> & _vol_total_strain;
-
-private:
-  const Real _b;
-  const Real _density;
+  const MaterialProperty<Real> & _scale_factor;
+  const MaterialProperty<RankTwoTensor> & _k_vis;
+  const MaterialProperty<Real> & _rho_f;
+  const MaterialProperty<Real> & _drho_dp_f;
+  const MaterialProperty<Real> & _drho_dT_f;
+  const MaterialProperty<Real> & _mu_f;
+  const MaterialProperty<Real> & _dmu_dp_f;
+  const MaterialProperty<Real> & _dmu_dT_f;
+  const MaterialProperty<RealVectorValue> & _g;
+  const MaterialProperty<Real> & _vol_strain_rate;
+  unsigned int _temperature_var;
+  std::vector<unsigned int> _disp;
 };
--- src/materials/TigerMechanicsMaterialM.C
+++ src/materials/TigerMechanicsMaterialM.C
@@ -36,6 +36,8 @@ validParams<TigerMechanicsMaterialM>()
         "specific density of rock for calculating bulk density (kg/m^3)");
   params.addParam<Real>("biot_coefficient", 1.0,
         "Biot's coefficient for poromechanics");
+  params.addRequiredCoupledVar("displacements",
+        "The displacements variables");
   params.addClassDescription("Mechanics material for mechanics kernels");
 
   return params;
@@ -47,8 +49,21 @@ TigerMechanicsMaterialM::TigerMechanicsMaterialM(const InputParameters & paramet
     _biot(declareProperty<Real>("biot_coefficient")),
     _n(getMaterialProperty<Real>("porosity")),
     _b(getParam<Real>("biot_coefficient")),
-    _density(getParam<Real>("specific_density"))
+    _density(getParam<Real>("specific_density")),
+    _ndisp(coupledComponents("displacements")),
+    _grad_disp(coupledGradients("displacements")),
+    _vol_strain_rate(declareProperty<Real>("volumetric_strain_rate_HM")),
+    _vol_total_strain(declareProperty<Real>("total_volumetric_strain_HM"))
 {
+  if (_ndisp != _mesh.dimension())
+    paramError("displacements", "The number of variables supplied must match the mesh dimension.");
+
+  if (_is_transient)
+    _grad_disp_old = coupledGradientsOld("displacements");
+
+  // set unused dimensions to zero
+  _grad_disp.resize(3, &_grad_zero);
+  _grad_disp_old.resize(3, &_grad_zero);
 }
 
 void
@@ -56,4 +71,22 @@ TigerMechanicsMaterialM::computeQpProperties()
 {
   _rho_b[_qp] = (1.0 - _n[_qp]) * _density;
   _biot[_qp] = _b;
+
+  RankTwoTensor A((*_grad_disp[0])[_qp],
+                  (*_grad_disp[1])[_qp],
+                  (*_grad_disp[2])[_qp]); // Deformation gradient
+
+  _vol_total_strain[_qp] = A.trace();
+
+  if (_is_transient)
+  {
+    RankTwoTensor Abar((*_grad_disp_old[0])[_qp],
+                       (*_grad_disp_old[1])[_qp],
+                       (*_grad_disp_old[2])[_qp]); // Old Deformation gradient
+    A -= Abar; // A = grad_disp - grad_disp_old
+    RankTwoTensor total_strain_increment = 0.5 * (A + A.transpose());
+    _vol_strain_rate[_qp] = total_strain_increment.trace() / _dt;
+  }
+  else
+    _vol_strain_rate[_qp] = 0.0;
 }
